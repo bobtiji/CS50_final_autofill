752fd79de807ebf76a36dc5154fd30ea050f0057
bob <guilbaultjp@gmail.com>
2022-03-12 11:47:31 -0500

    a

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
-
 from helpers import apology, login_required
 
 # create table to house users with unique userid
